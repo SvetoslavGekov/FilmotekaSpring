e57991428e0bcea7070cee7c9e09af572b25cc82
Svetoslav Gekov <svetoslav_gekov@abv.bg>
2018-04-26 09:38:31 +0300

    Added a FileController and implemented loading pictures from a file.

---
+++ MyProject/src/main/java/com/filmoteka/controller/FilesController.java
@@ -0,0 +1,48 @@
+package com.filmoteka.controller;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FilesController {
+	private static final String NO_IMAGE = "C:\\images\\no-photo.jpg";
+
+	@RequestMapping(value = "/getPic", method = RequestMethod.GET,produces = MediaType.IMAGE_JPEG_VALUE)
+	@ResponseBody
+	public byte[] getPicture(@RequestParam("pic") String pic) throws FileNotFoundException, IOException {
+		//Grab the file where the picture is saved at
+		File f = new File("C:\\images\\"+pic);
+		
+		//Check if the file exists
+		if(!f.exists()) {
+			//Assign the no image photo to the product without an image
+			f = new File(NO_IMAGE);
+		}
+		
+		byte[] byteArray = new byte[(int) f.length()];
+		//Write the picture in a byte array and return it
+		try(BufferedInputStream is = new BufferedInputStream(new FileInputStream(f));){
+			is.read(byteArray);
+//			OutputStream os = resp.getOutputStream();
+//			int b = is.read();
+//			while(b != -1) {
+//				os.write(b);
+//				b = is.read();
+//			}
+		}
+		
+		return byteArray;
+	}
+}
--- MyProject/src/main/webapp/WEB-INF/views/jsp/main.jsp
+++ MyProject/src/main/webapp/WEB-INF/views/jsp/main.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -45,5 +46,30 @@
 	<button class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-green">Sign In</button>
   </div>
 </div>
+
+	<!-- Contents -->
+	<div id = "content" class="w3-container w3-right" style="margin-top:7%">
+			<c:forEach var="entry" items="${mainPageProducts}">
+	  			<p><c:out value="${entry.key}"/></p>
+	  			<div class="w3-panel w3-padding w3-border w3-round-xxlarge">
+	  				<div class="w3-row-padding">
+	  					<p class="w3-panel w3-text-blue w3-wide"><b>${entry.key}</b></p>
+			  			<c:forEach var="product" items="${entry.value}">
+			  			<div class="w3-col" style="width:15%">
+			  				<div class = "w3-border-5 w3-border-black w3-metro-light-green w3-round-large" style="border:16px">
+			  					<p class = "w3-center w3-small w3-wide"><b>${product.name}</b></p>
+			  					<div class="w3-display-container w3-white">
+			  					<img src="getPic?pic=${product.poster}" class="w3-hover-opacity" style="width:100%; height:250px;">
+			  						<div class="w3-display-middle w3-display-hover">
+			  							<button class="w3-button w3-indigo w3-tiny">More Info</button>
+			  						</div>
+			  					</div>
+			  				</div>
+			  			</div>
+			  			</c:forEach>
+			  		</div>
+			  	</div>
+			</c:forEach>
+	</div>
 </body>
 </html>
